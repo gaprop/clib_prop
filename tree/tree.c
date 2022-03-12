59b38bc40ff1928ed471bca6a130b1e9e5df9d03
Anders Kildemand <anders.kildemand@gmail.com>
2022-03-12 21:53:18 +0100

    Added more to tree

--- tree/tree.c
+++ tree/tree.c
@@ -1,18 +1,40 @@
 #include <stdlib.h>
 #include "../utils.h"
+#include "tree.h"
 
 struct tree* tree_new() {
   struct tree* tree = malloc(sizeof(struct tree));
   tree->val = NULL;
   tree->left = NULL;
   tree->right = NULL;
+  tree->key = 0;
   return tree;
 } 
 
-void tree_push(struct tree* tree, void* val) {
-  if (tree->val == NULL) {
-    tree->val = val;
-  } else if (tree->left == NULL) {
+void tree_push(struct tree* tree, void* val, unsigned int key) {
+  struct tree* curr_tree = tree;
 
+  while (curr_tree->val != NULL) {
+    if (curr_tree->key < key) {
+      curr_tree = curr_tree->left;
+    } else if (curr_tree->key >= key) {
+      curr_tree = curr_tree->right;
+    }
   }
+
+  curr_tree->val = val;
+  curr_tree->key = key;
+  curr_tree->left = tree_new();
+  curr_tree->right = tree_new();
+  /* if (curr_tree->val == NULL) { */
+    /* curr_tree->val = val; */
+    /* curr_tree->key = key; */
+    /* curr_tree->left = tree_new(); */
+    /* curr_tree->right = tree_new(); */
+  /* }  */
+  /* if (tree->index < index) { */
+    /* tree_push(tree->left, val, index); */
+  /* } else if (tree->index >= index) { */
+    /* tree_push(tree->right, val, index); */
+  /* } */
 }
--- tree/tree.h
+++ tree/tree.h
@@ -1,13 +1,15 @@
 
 struct tree {
+  unsigned int key;
   void* val;
   struct tree* left;
   struct tree* right;
-}
+};
 
 struct tree* tree_new();
 
-void tree_push(struct tree* tree, void* val);
+// the compare value needs a different type, propably
+void tree_push(struct tree* tree, void* val, unsigned int key);
 
 void* tree_pop(struct tree* tree);
 
